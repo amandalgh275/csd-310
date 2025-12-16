9dcd3b185fd3b520c19ae574c07b3f982170810b
amandalgh275 <amandalgh275@gmail.com>
2025-12-15 20:46:45 -0500

    Add Module 2 assignment files

---
+++ module-2/greet_debug.py.py
@@ -0,0 +1,16 @@
+"""
+Simple Greeting Program with Debugging
+Author: Amanda Tirey
+CSD-325 Module 2 Assignment
+"""
+
+def greet():
+    """Function to print a greeting"""
+    print('Hello World!')
+
+# Main program execution
+if __name__ == "__main__":
+
+    
+    # Call the function
+    greet()
