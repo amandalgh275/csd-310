b89086a596710ac55759402154be840d9cc7ac5d
amandalgh275 <amandalgh275@gmail.com>
2026-02-26 20:09:51 -0500

    Add files via upload

---
+++ module-10/HelloWorld.py
@@ -0,0 +1,14 @@
+from logging import root
+import tkinter as tk
+
+class Root(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.label=tk.Label(self,text="HelloWorld",padx=5,pady=5)
+        self.label.pack()
+
+if __name__=="__main__":
+    root = Root()
+    root.mainloop()
+    
